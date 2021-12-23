ea81dbac5068b4fb866b3f2f30446fb683227072
qwami-vibes <qwaedjekum@gmail.com>
2021-12-23 14:45:43 +0000

    added firebase confiq

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { getFirestore } from "firebase/firestore/lite";
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+});
+
+export const auth = app.auth();
+
+export const db = getFirestore(app);
